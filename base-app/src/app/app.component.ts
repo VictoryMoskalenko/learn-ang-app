2e7a9209a2c39d549d69ffb76e8f45291a45962f
VictoryMoskalenko <vita.moskalenko@gmail.com>
2019-09-22 14:55:50 +0300

    binding added

--- base-app/src/app/app.component.ts
+++ base-app/src/app/app.component.ts
@@ -5,6 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'base-app';
+  title = 'Dinamic title';
+  number = 42;
+  arr = [1, 2, 3, 4, 5];
+
+  obj = { a: 1, b: {c: 2} }
+
+  // tslint:disable-next-line:max-line-length
+  img = 'https://banner2.kisspng.com/20180518/ptw/kisspng-react-logo-javascript-front-and-back-ends-user-int-5afef575942028.3034008315266584216067.jpg'
+
+  constructor() {
+    setTimeout( () => {
+      console.log('Timeout is over');
+      this.img = 'https://angular.io/assets/images/logos/angular/angular.png' 
+    }, 5000)
+  }
 }
