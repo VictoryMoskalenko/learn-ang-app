f6860e620c9512354c8171297332f8eb8ecd32dd
VictoryMoskalenko <vita.moskalenko@gmail.com>
2019-09-22 17:33:46 +0300

    two way binding without FormsModule

--- base-app/src/app/app.component.ts
+++ base-app/src/app/app.component.ts
@@ -7,32 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Dinamic title';
-  number = 42;
-  arr = [1, 2, 3, 4, 5];
 
-  obj = { a: 1, b: {c: 2} }
-
-  inputValue = '';
-
-  // tslint:disable-next-line:max-line-length
-  // img = 'https://banner2.kisspng.com/20180518/ptw/kisspng-react-logo-javascript-front-and-back-ends-user-int-5afef575942028.3034008315266584216067.jpg'
-
-  constructor() {
-
-  }
+  title = 'Initial';
 
   onInput(event: any) {
-    console.log('Event', event);
-    this.inputValue = (<HTMLInputElement>event.target).value
-  }
-
-  onBlur(str: string) {
-    this.inputValue = str
-  }
-
-  onClick() {
-    console.log('Click!')
+    this.title = event.target.value  
   }
 
 }
