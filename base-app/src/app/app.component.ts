21d1d97158d8b5e6952be286940c51ba95f360f3
VictoryMoskalenko <vita.moskalenko@gmail.com>
2019-09-23 13:53:12 +0300

    using *ngFor directive

--- base-app/src/app/app.component.ts
+++ base-app/src/app/app.component.ts
@@ -9,6 +9,23 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   // backgroundToggle = false;
-    toggle: any = false;
+  // toggle: any = false;
+
+  arr = [1, 1, 2, 3, 5, 8, 13];
+
+  objs = [
+    {title: 'Post 1', author: 'Some person', comments: [
+      {name: 'Max', text: 'Some text 1'},
+      {name: 'Max', text: 'Some text 2'},
+      {name: 'Max', text: 'Some text 3'},
+
+    ]},
+    {title: 'Post 2', author: 'Some person 2', comments: [
+      {name: 'Max 2', text: 'Some text 1'},
+      {name: 'Max 2', text: 'Some text 2'},
+      {name: 'Max 2', text: 'Some text 3'},
+
+    ]}
+  ] 
 
 }
