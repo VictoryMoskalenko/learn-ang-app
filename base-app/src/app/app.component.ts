8af4189327638f6005c3bb34f5ef8f0bbdb24a20
VictoryMoskalenko <vita.moskalenko@gmail.com>
2019-09-23 14:34:37 +0300

    using pipes

--- base-app/src/app/app.component.ts
+++ base-app/src/app/app.component.ts
@@ -11,21 +11,23 @@ export class AppComponent {
   // backgroundToggle = false;
   // toggle: any = false;
 
-  arr = [1, 1, 2, 3, 5, 8, 13];
+  // arr = [1, 1, 2, 3, 5, 8, 13];
 
-  objs = [
-    {title: 'Post 1', author: 'Some person', comments: [
-      {name: 'Max', text: 'Some text 1'},
-      {name: 'Max', text: 'Some text 2'},
-      {name: 'Max', text: 'Some text 3'},
+  // objs = [
+  //   {title: 'Post 1', author: 'Some person', comments: [
+  //     {name: 'Max', text: 'Some text 1'},
+  //     {name: 'Max', text: 'Some text 2'},
+  //     {name: 'Max', text: 'Some text 3'},
 
-    ]},
-    {title: 'Post 2', author: 'Some person 2', comments: [
-      {name: 'Max 2', text: 'Some text 1'},
-      {name: 'Max 2', text: 'Some text 2'},
-      {name: 'Max 2', text: 'Some text 3'},
+  //   ]},
+  //   {title: 'Post 2', author: 'Some person 2', comments: [
+  //     {name: 'Max 2', text: 'Some text 1'},
+  //     {name: 'Max 2', text: 'Some text 2'},
+  //     {name: 'Max 2', text: 'Some text 3'},
 
-    ]}
-  ] 
+  //   ]}
+  // ];
+  
+  now: Date = new Date();
 
 }
