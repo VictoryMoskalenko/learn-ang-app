d0e7bfb3844cd047403023d65b4a934a87c06260
VictoryMoskalenko <vita.moskalenko@gmail.com>
2019-10-15 17:22:11 +0300

    working with directives

--- basics-app/src/app/directives/style.directive.ts
+++ basics-app/src/app/directives/style.directive.ts
@@ -1,11 +1,31 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, Renderer2, HostListener, Input} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 
 export class StyleDirective {
+
+    @Input('appStyle') color: string = 'blue';
+    @Input() fontWeight = 'normal'
+
     constructor(private el: ElementRef, private r: Renderer2 ) {
-        this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+        // this.r.setStyle(this.el.nativeElement, 'color', 'blue');
+    }
+
+    @HostListener('click', ['$event.target']) onClick(event: Event) {
+        console.log(event);
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.r.setStyle(this.el.nativeElement, 'color', this.color);
+        this.r.setStyle(this.el.nativeElement, 'fontWeight', this.fontWeight);
+
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null);
+        this.r.setStyle(this.el.nativeElement, 'fontWeight', null);
+
     }
 }
